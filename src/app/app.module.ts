26c5b4bd76e8b414dd1a8e00b9089fb373f78701
aknutman <aknutman@gmail.com>
2021-09-03 21:33:58 +0700

    top-bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
-import { MatSliderModule } from '@angular/material/slider';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  imports: [BrowserModule, FormsModule, MatSliderModule],
-  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    RouterModule,
+
+    MatProgressBarModule,
+    MatToolbarModule
+  ],
+  declarations: [AppComponent, TopBarComponent, LoginComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
